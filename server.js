3a714a8818ca36358e4fb029919dc74bc301fc85
Shabala Yevhen <Genya@MacBookPro-Shabala-3.local>
2017-07-12 22:48:53 +0300

    small-change

--- server.js
+++ server.js
@@ -9,16 +9,11 @@ const app = express();
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+      return res.redirect(['http://', req.get('Host'), req.url].join(''));
     }
     next();
   }
 }
-
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
